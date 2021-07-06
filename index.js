27e915f33692c7101c28156d7ec46d3d91a8ca59
Aditya Singh <adirocksh@gmail.com>
2021-07-06 23:46:44 +0530

    new version

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App V2!')
+  res.send('Application version V2!')
 })
 
 app.get('/status', (req, res) => {
